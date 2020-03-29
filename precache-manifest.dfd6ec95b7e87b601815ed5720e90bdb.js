b9afa7e471e62ff2acecd8ddd1407d6fbb398872
vanamayorov <denatarelcom2105@gmail.com>
2020-03-29 19:34:48 +0300

    Updates

---
+++ precache-manifest.dfd6ec95b7e87b601815ed5720e90bdb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ab261f47f585d52c731a3d89a420e5",
+    "url": "/new_react_project/index.html"
+  },
+  {
+    "revision": "93750205ac15607d6579",
+    "url": "/new_react_project/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "472a624ce0a154235744",
+    "url": "/new_react_project/static/css/main.7988b4ac.chunk.css"
+  },
+  {
+    "revision": "93750205ac15607d6579",
+    "url": "/new_react_project/static/js/2.004f9411.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/new_react_project/static/js/2.004f9411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "472a624ce0a154235744",
+    "url": "/new_react_project/static/js/main.b0f9672d.chunk.js"
+  },
+  {
+    "revision": "11b8a9191e53751d7b08",
+    "url": "/new_react_project/static/js/runtime-main.7ca15d50.js"
+  }
+]);
